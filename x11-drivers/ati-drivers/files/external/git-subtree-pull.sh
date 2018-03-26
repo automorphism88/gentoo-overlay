7491c0dad230d6f33354979b5c4f5ff3478f4011
automorphism88 <adebeus@gmail.com>
2018-03-25 22:39:30 -0700

    x11-drivers/ati-drivers: Add merge commit message in subtree pull script

--- x11-drivers/ati-drivers/files/external/git-subtree-pull.sh
+++ x11-drivers/ati-drivers/files/external/git-subtree-pull.sh
@@ -3,5 +3,6 @@
     set -e
     CDPATH= cd -- "$(CDPATH= cd -- "$(dirname -- "$0")" && pwd)/../../../.."
     git subtree pull --prefix x11-drivers/ati-drivers/files/external/arch-AUR \
-        https://aur.archlinux.org/catalyst.git master --squash
+        https://aur.archlinux.org/catalyst.git master --squash \
+        -m 'x11-drivers/ati-drivers: merge updated AUR subtree'
 )
